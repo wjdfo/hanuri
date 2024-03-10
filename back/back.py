897df455273d839fe539d23b11cb4bb2dc53a780
JungRae Jo <wjdfoek3@knu.ac.kr>
2024-03-10 18:47:16 +0900

    server version

--- back/back.py
+++ back/back.py
@@ -38,5 +38,5 @@ def send():
 
 
 if __name__ == "__main__" :
-    app.run(port = 5000)
-    # app.run(debug = False, host = '0.0.0.0')
+    # app.run(port = 5000)
+    app.run(debug = False, host = '0.0.0.0')
--- front/my-app/src/Input.js
+++ front/my-app/src/Input.js
@@ -31,7 +31,7 @@ class Input extends React.Component {
 
     handleSubmit = async (event) => {
         try {
-            const response = await fetch("http://127.0.0.1:5000", {
+            const response = await fetch("https://3.37.143.40", {
                 method: 'POST',
                 headers: {
                 'Content-Type': 'application/json',
