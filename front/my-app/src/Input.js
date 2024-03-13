7b60e310d2a79968a77502e56a099b8065013f81
JungRae Jo <wjdfoek3@knu.ac.kr>
2024-03-13 20:25:26 +0900

    deploy version

--- front/my-app/src/Input.js
+++ front/my-app/src/Input.js
@@ -31,7 +31,7 @@ class Input extends React.Component {
 
     handleSubmit = async (event) => {
         try {
-            const response = await fetch("https://34.227.108.212", {
+            const response = await fetch("http://18.212.77.181/", {
                 method: 'POST',
                 headers: {
                 'Content-Type': 'application/json',
